5081f99e2465eb99a803d46e67f7c10de6cd6d00
ramirosevergnini <ramirosevergnini2020@gmail.com>
2024-04-26 15:41:40 -0300

    novo darkMode, melhorado.

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const modoNoturnoBotao = document.getElementById('modo-noturno-botao');
-const corpo = document.body;
-const header = document.querySelector('header');
-const main = document.querySelector('main');
-const footer = document.querySelector('footer');
-function darkMode() {
-    console.log('darkMode() foi chamada');
-    corpo.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode');
-    main.classList.toggle('dark-mode');
-    footer.classList.toggle('dark-mode');
-}
+const html = document.querySelector('html')
+const checkbox = document.getElementById('switch')
+checkbox.addEventListener('change', function()  {
+    html.classList.toggle('dark-mode')
+})
