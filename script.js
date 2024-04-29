ed5111d2df73a1f599eecf608437fd538cb77216
ramirosevergnini <ramirosevergnini2020@gmail.com>
2024-04-29 15:34:00 -0300

    portifolio quase pronto!

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const modoNoturnoBotao = document.getElementById('modo-noturno-botao');
 const html = document.querySelector('html')
 const checkbox = document.getElementById('switch')
+const hamburguer = document.getElementById('menuHamburguer')
+const menu = document.getElementById('menu')
+
 checkbox.addEventListener('change', function()  {
     html.classList.toggle('dark-mode')
 })
+function abrirMenu(){
+    if(menu.style.display == 'block'){
+        menu.style.display = 'none'
+    }else{
+        menu.style.display = 'block' 
+    }
+}
